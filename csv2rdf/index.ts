1432c28fb7a2799943bbcb8c4f6b8c0589870172
takanakahiko <takanakahiko@gmail.com>
2019-05-04 11:06:31 +0900

    CSV2RDF  にて $baseurl を全面的に使えるように修正 (#33)
    * incert commnet in csv2rdf
    
    * Allow $BASE_URL in setting-json
    
    * change schemaURL

--- csv2rdf/index.ts
+++ csv2rdf/index.ts
@@ -3,14 +3,17 @@ import * as fs from 'fs'
 import * as rimraf from 'rimraf'
 
 (async () => {
+  // virtuosoディレクトリを削除し，virtuoso/toLoadディレクトリを作成
   if(fs.existsSync('../virtuoso')) rimraf.sync('../virtuoso')
   fs.mkdirSync('../virtuoso')
   fs.mkdirSync('../virtuoso/toLoad')
 
+  // schema.ttl を作成
   // const csv2rdf_ = new Csv2rdf()
   // await csv2rdf_.load('./_data/ontology/prism-schema.json')
-  // await csv2rdf_.export('./virtuoso/toLoad/output.ttl')
+  // await csv2rdf_.export('./virtuoso/toLoad/schema.ttl')
 
+  // output.ttl(virtuosoにロードするデータ)を作成
   const csv2rdf = new Csv2rdf()
   await csv2rdf.load('../_data/convert-settings/pripara-characters-setting.json')
   await csv2rdf.export('../virtuoso/toLoad/output.ttl')

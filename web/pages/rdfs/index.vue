97a05ae2fd7407b6ebcec5ddb25e249b15cab6fc
takanakahiko <takanakahiko@gmail.com>
2019-05-04 18:08:33 +0900

    /rdfs にてクラス名をリスティング

--- web/pages/rdfs/index.vue
+++ web/pages/rdfs/index.vue
@@ -7,6 +7,11 @@
       </ul>
     </nav>
     <h1 class="title is-1">RDFs</h1>
-    <p>TODO</p>
+    <ul>
+      <li><a href="/rdfs/characters">Character</a></li>
+      <li><a href="/rdfs/episodes">Episode</a></li>
+      <li><a href="/rdfs/songs">Song</a></li>
+      <li><a href="/rdfs/lives">Live</a></li>
+    </ul>
   </div>
 </template>

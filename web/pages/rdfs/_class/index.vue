fde7f758a978ce3e1e161b38db54f852e1fb7119
takanakahiko <takanakahiko@gmail.com>
2019-05-06 12:47:05 +0900

    change path convert logic

--- web/pages/rdfs/_class/index.vue
+++ web/pages/rdfs/_class/index.vue
@@ -22,8 +22,7 @@ export default {
   components: { SparqlResponseTable },
   async asyncData({ $axios, params, error }) {
     const schemeBaseUrl = `https://prismdb.takanakahiko.me/prism-schema.ttl#` // これは環境変数でいいかも
-    const className =
-      params.class.charAt(0).toUpperCase() + params.class.slice(1, -1) // 先頭大文字にして,最期の's'を削除
+    const className = params.class.charAt(0).toUpperCase() + params.class.slice(1, 0) // 先頭を大文字にする
     const classUri = `${schemeBaseUrl}${className}`
     const typePredUri = `http://www.w3.org/1999/02/22-rdf-syntax-ns#type`
     const query = `SELECT ?URI

162cad0b2fa218c074d9b726d373004559652c35
takanakahiko <takanakahiko@gmail.com>
2019-05-06 23:26:39 +0900

    add arg arrayParameters to getProperties

--- restapi/routes/series.ts
+++ restapi/routes/series.ts
@@ -5,6 +5,9 @@ const router = Router();
 
 const className = `Series`
 const classBaseUri = "https://prismdb.takanakahiko.me/rdfs/series/"
+const arrayParameters = {
+    'hasEpisode': 'episodes'
+}
 
 router.get("/", async (req, res) => {
     const keys = await Sparql.getKeys(className, classBaseUri)
@@ -15,7 +18,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:key", async (req, res) => {
     const key = req.params.key
-    const properties = await Sparql.getProperties(key, classBaseUri)
+    const properties = await Sparql.getProperties(key, classBaseUri, arrayParameters)
     res.json(properties)
 })
 

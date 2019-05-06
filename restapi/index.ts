dbe0291f185c74442c2254076d2fee0f7b0a4881
takanakahiko <takanakahiko@gmail.com>
2019-05-06 22:19:26 +0900

    add indentation to restapi reoponse

--- restapi/index.ts
+++ restapi/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import routes from "./routes"
 
 const app = express();
+app.set('json spaces', 2)
 app.use("/", routes);
 app.listen(4567, () => {
     console.log(`listening on 4567`);

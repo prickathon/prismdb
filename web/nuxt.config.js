12b7474f6b1d48a0ba4efb3d1d78d8c5e0489062
takanakahiko <takanakahiko@gmail.com>
2019-05-06 12:55:46 +0900

    change lint option to fix:true

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -66,7 +66,10 @@ export default {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
+          options : {
+            fix : true
+          }
         })
       }
     }

4eb8fc3a140186c35c3345423941b508bf0f5e59
takanakahiko <takanakahiko@gmail.com>
2019-05-09 18:52:31 +0900

    add content of top-page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,34 +1,52 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
-      </card>
-
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/">Vue.js</a> and
-        <a href="http://bulma.io/">Bulma</a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+  <div class="">
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body has-text-centered">
+        <div class="container">
+          <h1 class="title is-1">PrismDB</h1>
+          <p>
+            PrismDB はプリティーシリーズの情報をまとめるプラットフォームです．
+            同人活動の一環として行われています．
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content container">
+        <h3 class="title is-3">Linked Open Data</h3>
+        <p>PrismDBはデータをLOD (Linked Open Data)として公開しています．</p>
+        <h4 class="title is-4">SPARQL</h4>
+        <p>
+          <a href="/rdfs">
+            WebやアプリからSPARQLで検索・利用可能なエンドポイント
+          </a>
+        </p>
+        <h4 class="title is-4">各種RDF</h4>
+        <p>
+          <a href="/rdfs">
+            SPARQLで使用できる各種URIを公開しています
+          </a>
+        </p>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content container">
+        <h3 class="title is-3">REST API</h3>
+        <p>PrismDBはデータをREST APIとして公開しています．</p>
+        <p>
+          <a href="https://github.com/prickathon/prismdb/tree/master/restapi">
+            REST API に関するドキュメント
+          </a>
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
   name: 'HomePage',
 
-  components: {
-    Card
-  }
+  components: {}
 }
 </script>

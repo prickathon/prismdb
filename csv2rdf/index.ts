9a0fe6de78e3fdf29918c622b024f44b0edd2adc
takanakahiko <takanakahiko@gmail.com>
2019-07-21 15:05:43 +0900

    migrate items path

--- csv2rdf/index.ts
+++ csv2rdf/index.ts
@@ -29,6 +29,6 @@ import * as rimraf from 'rimraf'
   await csv2rdf.load('../_data/convert-settings/songs-setting.json')
   await csv2rdf.load('../_data/convert-settings/pripara-lives-setting.json')
   await csv2rdf.load('../_data/convert-settings/prichan-lives-setting.json')
-  await csv2rdf.load('../_data/convert-settings/prichan-items-setting.json')
+  await csv2rdf.load('../_data/item/prichan-items-setting.json')
   await csv2rdf.export('../virtuoso/data/toLoad/output.ttl')
 })()

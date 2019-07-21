ba3ace702c579fb82686a4d79f5fa839b3038859
takanakahiko <takanakahiko@gmail.com>
2019-07-21 15:49:17 +0900

    migrate songs path

--- csv2rdf/index.ts
+++ csv2rdf/index.ts
@@ -26,7 +26,7 @@ import * as rimraf from 'rimraf'
   await csv2rdf.load('../_data/episode/ipp-episodes-setting.json')
   await csv2rdf.load('../_data/episode/prichan-episodes-setting.json')
   await csv2rdf.load('../_data/series/series-setting.json')
-  await csv2rdf.load('../_data/convert-settings/songs-setting.json')
+  await csv2rdf.load('../_data/song/songs-setting.json')
   await csv2rdf.load('../_data/convert-settings/pripara-lives-setting.json')
   await csv2rdf.load('../_data/convert-settings/prichan-lives-setting.json')
   await csv2rdf.load('../_data/item/prichan-items-setting.json')

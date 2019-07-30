f564e7bae9e0f069efc0b7f523911c6b00af817e
takanakahiko <takanakahiko@gmail.com>
2019-07-30 19:36:12 +0900

    replace page title from key to label

--- web/pages/rdfs/_class/_key.vue
+++ web/pages/rdfs/_class/_key.vue
@@ -13,7 +13,7 @@
           </li>
         </ul>
       </nav>
-      <h1 class="title is-1">{{ key }}</h1>
+      <h1 class="title is-1">{{ label }}</h1>
       <sparql-response-table :response="response" />
     </div>
   </section>
@@ -23,6 +23,16 @@
 import SparqlResponseTable from '~/components/SparqlResponseTable'
 export default {
   components: { SparqlResponseTable },
+  computed: {
+    label: function() {
+      let ret = ''
+      this.response.results.bindings.forEach(binding => {
+        const labelUri = 'https://www.w3.org/2000/01/rdf-schema#label'
+        if (binding.Property.value === labelUri) ret = binding.Value.value
+      })
+      return ret
+    }
+  },
   async asyncData({ $axios, params, error }) {
     const rdfsBaseUrl = `https://prismdb.takanakahiko.me/rdfs/` // これは環境変数でいいかも
     const subjectUrl = `${rdfsBaseUrl}${params.class}/${params.key}`

ee10ad88a1dc5d3b9356b992111f93a64790b19c
takanakahiko <takanakahiko@gmail.com>
2019-10-04 14:53:13 +0900

    fix #175 (#177)

--- csv2rdf/index.ts
+++ csv2rdf/index.ts
@@ -2,7 +2,7 @@ import Csv2rdf from './csv2rdf'
 import * as fs from 'fs'
 import * as rimraf from 'rimraf'
 
-(async () => {
+const main = async () => {
   // virtuosoディレクトリを削除し，virtuoso/toLoadディレクトリを作成
   if(fs.existsSync('../virtuoso/data')) rimraf.sync('../virtuoso/data')
   fs.mkdirSync('../virtuoso/data')
@@ -36,4 +36,10 @@ import * as rimraf from 'rimraf'
   await csv2rdf.load('../_data/item/prichan-items-setting.json')
   await csv2rdf.load('../_data/shop/prichan-shop-setting.json')
   await csv2rdf.export('../virtuoso/data/toLoad/output.ttl')
+}
+
+(async () => {
+  await main().catch(e => {
+    process.exit(1);
+  })
 })()

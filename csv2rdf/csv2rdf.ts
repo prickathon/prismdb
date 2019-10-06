c76d5fbd33907101bbe13210fd8e8eb13e3700af
takanakahiko <takanakahiko@gmail.com>
2019-10-06 15:05:36 +0900

    俺の答えはこれや

--- csv2rdf/csv2rdf.ts
+++ csv2rdf/csv2rdf.ts
@@ -116,7 +116,7 @@ const getPatternResult = (keyAndPattern:KeyAndPattern, row: Object) => {
 
 const subjectKey = (row: Object, setting: Setting) => {
     const val = setting.subjectKey ? getPatternResult(setting.subjectKey, row) : row["key"] as string
-    return encodeURI(val)
+    return val.normalize("NFKC")
 }
 
 const getLebel = (row: Object, setting: Setting) => {

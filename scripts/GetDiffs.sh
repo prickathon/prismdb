104f486098881667e0d66409ecad0c88a76a9f60
takanakahiko <takanakahiko@gmail.com>
2019-11-28 11:18:39 +0900

    comment diffs

---
+++ scripts/GetDiffs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x # verbose mode
+set -e # stop executing after error
+
+diff="$(diff --recursive --new-file --unified=0 /tmp/old /tmp/new || true)" # 'or true' because a non-identical diff outputs 1 as the exit status
+
+comment="\`\`\`diff
+$diff
+\`\`\`"
+
+echo $comment > ./comment.txt

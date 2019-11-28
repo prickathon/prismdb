4ec206af377cd6429defc2519ce22d5ee8b1747a
takanakahiko <takanakahiko@gmail.com>
2019-11-28 17:34:58 +0900

    しっかり開業するようにした

--- scripts/GetDiffs.sh
+++ scripts/GetDiffs.sh
@@ -5,8 +5,8 @@ set -e # stop executing after error
 
 diff="$(diff --recursive --new-file --unified=0 /tmp/old /tmp/new || true)" # 'or true' because a non-identical diff outputs 1 as the exit status
 
-comment="\`\`\`diff
+cat > diff.md << "EOF"
+```diff
 $diff
-\`\`\`"
-
-echo $comment > ./diff.md
+```
+EOF

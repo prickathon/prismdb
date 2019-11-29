605b86917d88d56d9764a62e8bbbefee1e1a22bc
takanakahiko <takanakahiko@gmail.com>
2019-11-29 14:58:30 +0900

    うん

--- scripts/GetDiffs.sh
+++ scripts/GetDiffs.sh
@@ -5,7 +5,7 @@ set -e # stop executing after error
 
 diff="$(diff --new-file --unified=0 base/base.ttl rdfs/output.ttl || true)" # 'or true' because a non-identical diff outputs 1 as the exit status
 
-cat $diff
+cat "$diff"
 
 cat > diff.md << EOS
 \`\`\`diff

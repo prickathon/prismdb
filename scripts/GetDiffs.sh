161eeb93d001e6ef007ab5bb8bc61d3e4d793586
takanakahiko <takanakahiko@gmail.com>
2019-11-29 14:54:22 +0900

    fix path

--- scripts/GetDiffs.sh
+++ scripts/GetDiffs.sh
@@ -3,7 +3,7 @@
 set -x # verbose mode
 set -e # stop executing after error
 
-diff="$(diff --new-file --unified=0 /base/base.ttl /rdfs/output.ttl || true)" # 'or true' because a non-identical diff outputs 1 as the exit status
+diff="$(diff --new-file --unified=0 base/base.ttl rdfs/output.ttl || true)" # 'or true' because a non-identical diff outputs 1 as the exit status
 
 cat $diff
 

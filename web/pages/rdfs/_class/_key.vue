12c52f530d2f91b4a7c6b168bb3a4d01a0134f8a
takanakahiko <takanakahiko@gmail.com>
2020-03-15 12:01:12 +0900

    add head to page

--- web/pages/rdfs/_class/_key.vue
+++ web/pages/rdfs/_class/_key.vue
@@ -29,6 +29,15 @@ import { SparqleResponse } from '~/types/SparqleResponse.d.ts'
 
 export default Vue.extend({
   components: { SparqlResponseTable },
+  head () {
+    return {
+      title: `${this.label} - PrismDB`,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: `prismdb の「${this.label}」のページです` }
+      ]
+    }
+  },
   computed: {
     label (): string {
       let ret = ''

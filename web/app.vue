24f55869e787176340ad0d163e8809f216b0858f
takanakahiko <takanakahiko@gmail.com>
2023-10-08 22:17:30 +0900

    https://nuxt.com/docs/migration/meta#meta-tags

---
+++ web/app.vue
@@ -0,0 +1,15 @@
+<script setup>
+useHead({
+  title: 'PrismDB',
+  meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'prismdb は プリティーシリーズの二次創作です' }
+  ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+})
+</script>
+
+<template>
+  <NuxtPage />
+</template>
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -4,19 +4,6 @@ export default defineNuxtConfig({
   mode: true,
 
   /*
-   ** Headers of the page
-   */
-  head: {
-    title: 'PrismDB',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'prismdb は プリティーシリーズの二次創作です' }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-
-  /*
    ** Customize the progress-bar color
    */
   loading: { color: '#fff' },

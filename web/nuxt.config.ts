2d997bdcacbf2854ef7881704f436b77d15d7b3f
takanakahiko <takanakahiko@gmail.com>
2023-10-08 22:19:27 +0900

    config simplified

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,41 +1,10 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  mode: true,
-
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-
-  /*
-   ** Global CSS
-   */
-  css: [
-  ],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-  ],
-
   routeRules: {
     '/sparql': { proxy: process.env.SPARQL_ENDPOINT_URL },
   },
-
   typescript: {
     typeCheck: true,
-    ignoreNotFoundWarnings: true
   },
-
-  /*
-   ** Build configuration
-   */
-  build: {
-  },
-  buildModules: [],
-  vite: {
-    devBundler: 'legacy'
-  }
 })

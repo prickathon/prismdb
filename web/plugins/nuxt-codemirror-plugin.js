fd28633a6af7ba85c8b5e3f6a490160111b8dcf2
takanakahiko <takanakahiko@gmail.com>
2023-10-08 19:10:15 +0900

    migrate codemirror from v5 to v6

--- web/plugins/nuxt-codemirror-plugin.js
+++ web/plugins/nuxt-codemirror-plugin.js
@@ -1,9 +1,10 @@
 import { defineNuxtPlugin } from "#app";
 import VueCodemirror from 'vue-codemirror'
-
-// language
-import 'codemirror/mode/sparql/sparql.js'
+import { StreamLanguage } from "@codemirror/language"
+import { sparqle } from "@codemirror/legacy-modes/mode/sparql"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.Vue.use(VueCodemirror)
+  nuxtApp.Vue.use(VueCodemirror, {
+    extensions: [StreamLanguage.define(sparqle)]
+  })
 });

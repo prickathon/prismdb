a11fdcadbeb32e8955433f6d86dcf07ed6950333
takanakahiko <takanakahiko@gmail.com>
2023-10-08 23:36:30 +0900

    fix typos of plugins/nuxt-codemirror-plugin.ts

--- web/plugins/nuxt-codemirror-plugin.ts
+++ web/plugins/nuxt-codemirror-plugin.ts
@@ -1,10 +1,10 @@
 import { defineNuxtPlugin } from "#app";
 import VueCodemirror from 'vue-codemirror'
 import { StreamLanguage } from "@codemirror/language"
-import { sparqle } from "@codemirror/legacy-modes/mode/sparql"
+import { sparql } from "@codemirror/legacy-modes/mode/sparql"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.Vue.use(VueCodemirror, {
-    extensions: [StreamLanguage.define(sparqle)]
+  nuxtApp.vueApp.use(VueCodemirror, {
+    extensions: [StreamLanguage.define(sparql)]
   })
 });

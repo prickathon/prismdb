c6781ba59eb92c48a996f553d6b21768d9e5d1ce
takanakahiko <takanakahiko@gmail.com>
2023-10-10 11:51:04 +0900

    remove useHead of app.vue

--- web/app.vue
+++ web/app.vue
@@ -1,15 +1,3 @@
-<script setup>
-useHead({
-  title: 'PrismDB',
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { hid: 'description', name: 'description', content: 'prismdb は プリティーシリーズの二次創作です' }
-  ],
-  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-})
-</script>
-
 <template>
   <NuxtLayout>
     <NuxtPage />
